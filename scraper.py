acce006e4ee05fb08080ca630e07dfa1b692a780
Eyal Levin <60202457+babajaj@users.noreply.github.com>
2020-11-26 22:10:34 -0500

    restart

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 
 import numpy as np
-
+from PIL import Image
+import os
 import json
 #   list of profiles of length 103. We tried to somewhat balance male/female
 #   accounts and get some but not too many types of accounts .
@@ -17,30 +18,41 @@ profiles = ["arianagrande", "therock", "kyliejenner", "selenagomez", "kimkardash
             "chrisburkard", "thebodycoach", "emilyskyefit", "thebucketlistfamily", "minimalistbaker", "demibagby", "thesharkdaymond", "meghanrienks"]
 
 
-# def recent_100_pics(username):
-#     """With the input of an account page, scrape the 100 most recent posts urls"""
-#     url = "https://www.instagram.com/" + username + "/"
-#     browser = Chrome()
-#     browser.get(url)
-#     post = 'https://www.instagram.com/p/'
-#     post_links = []
-#     i = 0
-#     while len(post_links) < 25:
-#         print(i)
-#         i+=1
-#         links = [a.get_attribute('href') for a in browser.find_elements_by_tag_name('a')]
-#         for link in links:
-#             if post in link and link not in post_links:
-#                 post_links.append(link)
-#         scroll_down = "window.scrollTo(0, document.body.scrollHeight);"
-#         browser.execute_script(scroll_down)
-#     else:
-#         return post_links[:25]
 
 
 
-with open('profiles.txt', 'w') as filehandle:
-    for listitem in profiles:
-        filehandle.write('%s\n' % listitem)
+
+# #run this to get the profiles:
+# with open('profiles.txt', 'w') as filehandle:
+#     for listitem in profiles:
+#         filehandle.write('%s\n' % listitem)
+
+
+def load_data(profiles):
+    folder_index = 0
+    get_image = True
+    image = None
+    caption = None
+    images = []
+    captions = []
+    names = []
+    for name in profiles:
+        folder = "data/" + name
+        arr = os.listdir(folder)
+        for file in arr:
+            if get_image:
+                image = Image.open(folder + '/' + file)
+            else:
+                if file.endswith(".txt"):
+                    with open(file, 'r') as f:
+                        caption = f.read().replace('\n', '')
+            folder_index += 1
+            images.append(image)
+            captions.append(caption)
+            names.append(name)
+    return names, images, captions 
+
+    
+
 
     
