08ea5b3fbf93e67bdb8fda4a256062d021c49150
tbers22 <60243334+tbers22@users.noreply.github.com>
2020-11-29 22:53:19 -0500

    preprocessing and model outline

---
+++ model.py
@@ -0,0 +1,97 @@
+from preprocess import tokenize, pre_image
+from unscraper import load_data
+
+class Model(tf.keras.Model):
+    def __init__(self, vocab_size):
+        """
+        The Model class predicts the next character in a sequence.
+
+        :param vocab_size: The number of unique characters in the data
+        """
+
+        
+
+
+
+    def call(self, inputs, initial_state):
+        """
+        :param inputs: character ids of shape (batch_size, window_size)
+        :return: the batch element probabilities as a tensor, a final_state (Note 1: If you use an LSTM, the final_state will be the last two RNN outputs, 
+        Note 2: We only need to use the initial state during generation)
+        using LSTM and only the probabilites as a tensor and a final_state as a tensor when using GRU 
+        """
+
+    def loss(self, probs, labels):
+        """
+
+        :param logits: a matrix of shape (batch_size, window_size, vocab_size) as a tensor
+        :param labels: matrix of shape (batch_size, window_size) containing the labels
+        :return: the loss of the model as a tensor of size 1
+        """
+
+def train(model, train_inputs, train_labels):
+    """
+    Runs through one epoch - all training examples.
+
+    :param model: the initilized model to use for forward and backward pass
+    :param train_inputs: train inputs (all inputs for training) of shape (num_inputs,)
+    :param train_labels: train labels (all labels for training) of shape (num_labels,)
+    :return: None
+    """
+   
+    print(train_labels.shape)
+    remainderInputs = len(train_inputs) % model.window_size
+    currInputs = train_inputs[:-remainderInputs]
+    
+    remainderLabels = len(train_labels) % model.window_size
+    currLabels = train_labels[:-remainderLabels]
+    
+    currInputs = tf.reshape(currInputs,(-1,model.window_size))
+    currLabels = tf.reshape(currLabels,(-1,model.window_size))
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    for i in range(0, len(currInputs)//model.batch_size): 
+        inputs = currInputs[i*model.batch_size:(i+1)* model.batch_size]
+        labels = currLabels[i*model.batch_size:(i+1)* model.batch_size]
+        with tf.GradientTape() as tape:
+            predictions = model.call(inputs,None) 
+            loss = model.loss(predictions, labels)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    return None
+    
+
+
+def test(model, test_inputs, test_labels):
+    """
+    Runs through one epoch - all testing examples
+
+    :param model: the trained model to use for prediction
+    :param test_inputs: train inputs (all inputs for testing) of shape (num_inputs,)
+    :param test_labels: train labels (all labels for testing) of shape (num_labels,)
+    :returns: perplexity of the test set
+    """
+    
+
+
+def generate_sentence(word1, length, vocab, model, sample_n=10):
+    """
+    Takes a model, vocab, selects from the most likely next word from the model's distribution
+
+    :param model: trained RNN model
+    :param vocab: dictionary, word to id mapping
+    :return: None
+    IDK IF THIS ONE IS NEEDED
+    """
+
+ 
+
+
+def main():
+    data = load_data("captions.json","images.npy")
+    captions = tokenize(data[0])
+    images = pre_image(data[1])
+
+if __name__ == '__main__':
+    main()
+
+   
--- preprocess.py
+++ preprocess.py
@@ -1,21 +1,38 @@
-from keras.layers import (LSTM, Embedding, Input, Dense, Dropout)
-from keras.models import Model
-from keras.optimizers import Adam
-from keras.preprocessing.image import img_to_array, load_img
-from keras.preprocessing.text import Tokenizer
-from keras.applications.vgg16 import VGG16, preprocess_input
-from keras.preprocessing.sequence import pad_sequences
-from unscrapper.py import load_data
-def tokenize:
-    file1 = open(train_file, 'r') 
-    Lines = file1.readlines()
-    training_data =[]
-    x = 0
+
+
+def tokenize(captions):
+    """
+    :param captions: Captions of all the images
+    :return: Tuple of train (1-d list or array with each character from captions in id form), vocabulary (Dict containg word->index mapping)
+    """
     vocab_dict = dict()
-    for line in Lines: 
-        arr = line.strip().split()
-        for i in range(len(arr)):
-            if(arr[i] not in vocab_dict):
-                vocab_dict[arr[i]] = x
+    x = 1
+    #stop word is 0
+    training_data = []
+    counter = 0
+    string = ""
+    for line in captions:
+        for c in line:
+            if c == "\\":
+                string = "\\"
+                counter += 1
+            elif counter > 0:
+                string += c
+                counter += 1
+            elif counter == 6 and (string not in vocab_dict) :
+                counter = 0
+                vocab_dict[string] = x
+                string = ""
                 x = x + 1
-            training_data.append(vocab_dict[arr[i]])
+            elif c not in vocab_dict:
+                vocab_dict[c] = x
+                x = x + 1
+            training_data.append(vocab_dict[c])
+        training_data.append(0)
+    return training_data, vocab_dict
+    print(len(training_data))
+
+
+captions = [
+    "rum punch + the beach ", "hello fall \ud83d\udc9a "]
+print(tokenize(captions))
